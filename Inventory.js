96a17f3a3daf6fdfded7d17c1bc61e36358da684
Hemanthuppal <131530809+Hemanthuppal@users.noreply.github.com>
2025-09-12 16:08:22 +0530

    inventory file

--- Inventory.js
+++ Inventory.js
@@ -161,7 +161,7 @@ app.post('/api/products', async (req, res) => {
 
   try {
     for (const productDetails of productDetailsArray) {
-      const { main_category, sub_category, selected_product, size, hsncode, listprice, moq, batch } = productDetails;
+      const { main_category, sub_category, selected_product, size, hsncode, listprice, gstrate,moq, batch } = productDetails;
 
       // Validate required fields
       if (!selected_product || !main_category || !sub_category || !size || !hsncode || !listprice || !moq || !batch) {
@@ -177,8 +177,8 @@ app.post('/api/products', async (req, res) => {
       // Insert product details into the database
       await db.query(`
         INSERT INTO product_details (maincategory_id, subcategory_id, product_id, size, hsncode, gstrate, listprice, moq, batch)
-        VALUES (?, ?, ?, ?, ?, 18.00, ?, ?, ?)
-      `, [main_category, sub_category, selected_product, size, hsncode, listprice, moq, batch]);
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+      `, [main_category, sub_category, selected_product, size, hsncode,gstrate, listprice, moq, batch]);
     }
 
     res.status(201).json({ message: 'Products added successfully' });
